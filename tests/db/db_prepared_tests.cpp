6fef19e2bf54c3f62a30385a89e5fe1f1d96184e
RyanVNgo <vanryan711@gmail.com>
2026-04-23 13:20:59 -0700

    database execute_prepared tests

---
+++ tests/db/db_prepared_tests.cpp
@@ -0,0 +1,102 @@
+
+#include <filesystem>
+#include <format>
+
+#include <gtest/gtest.h>
+
+#include "db_utils.h"
+
+#include "db/database.h"
+
+
+TEST(DatabaseTests, db_prepared_valid) {
+    const char* db_filename = "db_prepared_valid.sqlite";
+    std::unique_ptr<SQLiteDatabase> test_db;
+    EXPECT_NO_THROW(test_db = std::make_unique<SQLiteDatabase>(db_filename));
+    
+    // Setup table
+    auto db = util_open_database(db_filename);
+    const std::string table_name = "test_table";
+    const std::string setup_query = std::format(
+        "CREATE TABLE IF NOT EXISTS {} ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "message TEXT NOT NULL,"
+        "level TEXT NOT NULL,"
+        "source TEXT,"
+        "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP"
+        ");",
+        table_name
+    );
+    ASSERT_TRUE(util_exec_query(db.get(), setup_query.c_str()));
+
+    // Test valid queries
+    const std::vector<std::pair<std::string, Row>> valid_queries = {
+        {
+            std::format("INSERT INTO {} (message, level, source) VALUES (?, ?, ?);", table_name),
+            {"test log", "INFO", "tests"}
+        },
+        {
+            std::format("INSERT INTO {} (message, level) VALUES (?, ?);", table_name),
+            {"test log", "INFO"}
+        }
+    };
+    for (const auto& [query, row] : valid_queries) {
+        ASSERT_TRUE(test_db->execute_prepared(query, row));
+    }
+
+    EXPECT_TRUE(util_validate_row_count(db.get(), table_name.c_str(), valid_queries.size()));
+
+    EXPECT_TRUE(std::filesystem::exists(db_filename));
+    std::filesystem::remove(db_filename);
+}
+
+TEST(DatabaseTests, db_prepared_invalid) {
+    const char* db_filename = "db_prepared_invalid.sqlite";
+    std::unique_ptr<SQLiteDatabase> test_db;
+    EXPECT_NO_THROW(test_db = std::make_unique<SQLiteDatabase>(db_filename));
+    
+    // Setup table
+    auto db = util_open_database(db_filename);
+    const std::string table_name = "test_table";
+    const std::string setup_query = std::format(
+        "CREATE TABLE IF NOT EXISTS {} ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "message TEXT NOT NULL,"
+        "level TEXT NOT NULL,"
+        "source TEXT,"
+        "timestamp DATETIME DEFAULT CURRENT_TIMESTAMP"
+        ");",
+        table_name
+    );
+    ASSERT_TRUE(util_exec_query(db.get(), setup_query.c_str()));
+
+    // Test valid queries
+    const std::vector<std::pair<std::string, Row>> valid_queries = {
+        {
+            std::format("INSERT INTO {} (message, source, level) VALUES (?, ?, ?);", table_name),
+            {"test log", "tests"}
+        },
+        {
+            std::format("INSERT INTO {} (message, level, source) VALUES (?, ?);", table_name),
+            {"test log", "INFO"}
+        },
+        {
+            std::format("INSERT INTO {} (message, level, source) VALUES (?, ?);", table_name),
+            {"test log", "INFO", "tests"}
+        },
+        {
+            std::format("INSERT INTO {} (message, level) VALUES (?, ?, ?);", table_name),
+            {"test log", "INFO", "tests"}
+        }
+    };
+    for (const auto& [query, row] : valid_queries) {
+        ASSERT_FALSE(test_db->execute_prepared(query, row)) << query;
+    }
+
+    EXPECT_TRUE(util_validate_row_count(db.get(), table_name.c_str(), 0));
+
+    EXPECT_TRUE(std::filesystem::exists(db_filename));
+    std::filesystem::remove(db_filename);
+}
+
+

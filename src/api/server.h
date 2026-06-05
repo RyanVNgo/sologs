91796e3880753e9c678d6e278295dae3767e2299
RyanVNgo <vanryan711@gmail.com>
2026-06-04 17:31:43 -0700

    remove httplib implementations

--- src/api/server.h
+++ src/api/server.h
@@ -2,7 +2,6 @@
 #pragma once
 
 
-#include <httplib.h>
 #include <drogon/HttpAppFramework.h>
 #include <drogon/HttpController.h>
 
@@ -10,59 +9,6 @@
 #include "auth_service.h"
 #include "key_service.h"
 
-
-class SOLogSServer {
-    public:
-        explicit SOLogSServer(
-                ILogService& log_service,
-                IAuthorizer& authorizer,
-                IAuthenticator& authenticator,
-                IKeyService& key_service
-        );
-
-        auto start(int port) -> void;
-
-        auto stop() -> void;
-
-    private:
-        auto get_health_handler(
-                const httplib::Request& req,
-                httplib::Response& res
-        ) -> void;
-        
-        auto post_logs_handler(
-                const httplib::Request& req,
-                httplib::Response& res
-        ) -> void;
-
-        auto get_logs_handler(
-                const httplib::Request& req,
-                httplib::Response& res
-        ) -> void;
-
-        auto post_auth_handler(
-                const httplib::Request& req,
-                httplib::Response& res
-        ) -> void;
-
-        auto parse_auth_key(
-                const httplib::Request& req
-        ) const -> std::string;
-
-        auto authorize_user(
-                const httplib::Request& req,
-                httplib::Response& res,
-                const std::vector<Permissions>& perms,
-                const PermissionMode& mode
-        ) -> bool;
-
-        httplib::Server server_;
-        ILogService& log_service_;
-        IAuthorizer& authorizer_;
-        IAuthenticator& authenticator_;
-        IKeyService& key_service_;
-
-};
 
 class SOLogSServerDrogon {
     public:
